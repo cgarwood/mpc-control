eda7f1ba8937cd9c5595c00273802a11ffb77158
Jay Oswald <jozzie52@gmail.com>
2018-08-04 09:26:21 +1000

    adding ws_port to server-config.example.js

--- server/server-config.example.js
+++ server/server-config.example.js
@@ -2,6 +2,9 @@
 module.exports = {
 	// Port for HTTP server the tablets will connect to
 	http_port : 80,
+
+	// Websocket Port that the website connects to, needs to be the same as in config.js
+	ws_port : 8082,
 	
 	// IP and port for the Martin MxManager Telnet Server
 	mxmanager_ip : "127.0.0.1",
